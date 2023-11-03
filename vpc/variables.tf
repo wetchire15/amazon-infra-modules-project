90d22230815faae49b7c11032ea8ac82ff13bcb8
Fran <wetchire1@gmail.com>
2023-11-02 22:00:46 -0400

    adding ec2,s3,vpc modules

---
+++ vpc/variables.tf
@@ -0,0 +1,23 @@
+variable "cidr_block" {
+    type = string
+    description = "vpc cidr range"
+    default = "10.0.0.0/16"
+}
+
+variable "vpc_name" {
+    type = string
+    description = "vpc name"
+    default = "amazon-vpc"
+}
+
+variable "subnet1_cidr" {
+    type = string
+    description = "subnet1 cidr range"
+    default = "10.0.1.0/24"
+}
+
+variable "subnet1_name" {
+    type = string
+    description = "subnet name"
+    default = "amazon-vpc-subnet1"
+}
